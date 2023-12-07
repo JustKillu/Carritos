573d98d118990f1c8c651f2d00cfac370b1a106d
JustKillu <nightcorekillu@gmail.com>
2023-12-07 01:20:22 -0400

    Base

--- frontend/src/components/Inicio.jsx
+++ frontend/src/components/Inicio.jsx
@@ -1,26 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 function Inicio() {
-  const [modelos, setModelos] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:3000/modelos')
-      .then(response => response.json())
-      .then(data => setModelos(data))
-      .catch(error => console.error('Error:', error));
-  }, []);
-
+  console.log('El componente Inicio se ha renderizado');
   return (
     <div>
-      <h1>Inicio</h1>
-      {modelos ? (
-        <div>
-          <h2>Modelos:</h2>
-          <pre>{JSON.stringify(modelos, null, 2)}</pre>
-        </div>
-      ) : (
-        <p>Cargando modelos...</p>
-      )}
+      <h1>¡Bienvenido a la página de inicio!</h1>
+      <p>Esta es la página de inicio de nuestra aplicación. ¡Gracias por visitarnos!</p>
     </div>
   );
 }
