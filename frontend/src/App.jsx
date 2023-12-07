ca03b7989f152a4b2139997976a6fb67ac50dbb8
JustKillu <nightcorekillu@gmail.com>
2023-12-06 23:53:27 -0400

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Inicio from './components/Inicio.jsx';
-import Comprar from './components/Comprar.jsx';
 import Login from './components/Login.jsx';
-import Register from './components/Register.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Inicio />} />
-        <Route path="/comprar" element={<Comprar />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
