f76c81318fbfe81d029b61ce9f1d55719b82b3f5
JustKillu <nightcorekillu@gmail.com>
2023-12-08 03:14:17 -0400

    Mas rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,25 @@ import Inicio from './components/Inicio.jsx';
 import Login from './components/Login.jsx';
 import Navb from './components/Navbar.jsx';
 import Modelos from './components/Modelos.jsx';
+import Register from './components/Register.jsx';
+import Comprar from './components/Comprar.jsx';
+import Pedidos from './components/Pedidos.jsx';
+import MisPedidos from './components/MisPedidos.jsx';
 
 function App() {
   return (
-    <>
-    <>
-    <Navb />
-    </>
     <Router>
+      <Navb />
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/login" element={<Login />} />
         <Route path= "/Modelos" element={<Modelos />} />
+        <Route path= "/register" element={<Register />} />
+        <Route path= "/comprar" element={<Comprar />} />
+        <Route path= "/pedidos" element={<Pedidos />} />
+        <Route path= "/mispedidos" element={<MisPedidos />} />
       </Routes>
     </Router>
-    </>
   );
 }
 
