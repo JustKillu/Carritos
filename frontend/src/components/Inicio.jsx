73a4371636c8c79da35816e7b754c1a62f14fdb5
JustKillu <nightcorekillu@gmail.com>
2023-12-08 16:06:54 -0400

    a

--- frontend/src/components/Inicio.jsx
+++ frontend/src/components/Inicio.jsx
@@ -18,9 +18,7 @@ function Inicio() {
         tecnología para mejorar la velocidad, la eficiencia y la seguridad.
       </p>
       </div>
-      <div className="secciones">
-          <h2> Nuestras opciones </h2>
-      </div>
+    
       </div>
   );
 }
