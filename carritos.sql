a9e3f643c3032904431221e2f2785d3e0eec6cde
JustKillu <nightcorekillu@gmail.com>
2023-12-08 03:16:20 -0400

    Se crearon mas tablas

--- carritos.sql
+++ carritos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-12-2023 a las 02:19:25
+-- Tiempo de generación: 08-12-2023 a las 08:15:55
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `compras`
+--
+
+CREATE TABLE `compras` (
+  `id` int(100) NOT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `Apellido` varchar(100) NOT NULL,
+  `Correo` varchar(100) NOT NULL,
+  `Carro` varchar(100) NOT NULL,
+  `Estado` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `compras`
+--
+
+INSERT INTO `compras` (`id`, `Nombre`, `Apellido`, `Correo`, `Carro`, `Estado`) VALUES
+(2, 'Juan', 'Perez', 'juan.perez@example.com', ' ', 'En espera'),
+(3, 'Juan', 'Perez', 'j ample.com', ' ', 'En espera'),
+(4, 'pedro', 'sanchez', '2222@gmailc.om', '5', 'Entregado'),
+(5, 'Nombre', 'apellido', 'correo@gmail.com', '5', 'En espera'),
+(7, '123', '123', '12312312@gmail.com', '6', 'Entregado');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `modelos`
 --
 
@@ -31,16 +57,19 @@ CREATE TABLE `modelos` (
   `id` int(100) NOT NULL,
   `Modelo` varchar(100) NOT NULL,
   `Precio` varchar(100) NOT NULL,
-  `Fecha` varchar(100) NOT NULL
+  `Fecha` varchar(100) NOT NULL,
+  `Img` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `modelos`
 --
 
-INSERT INTO `modelos` (`id`, `Modelo`, `Precio`, `Fecha`) VALUES
-(1, 'CORVETTE STINGRAY', '$68,300', '2024'),
-(4, 'CO123S11231RAY', '12312300', '2024');
+INSERT INTO `modelos` (`id`, `Modelo`, `Precio`, `Fecha`, `Img`) VALUES
+(1, 'CORVETTE STINGRAY', '$68,300', '2024', 'https://hips.hearstapps.com/hmg-prod/images/2024-chevrolet-corvette-e-ray-286-1673906201.jpg'),
+(5, 'Huracán STO RWD ', '338.577 €', '2021', 'https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/news/2020/11_18_sto/sto_cover.jpg'),
+(6, 'FERRARI F40', '2.400.000 €', '1989', 'https://img.remediosdigitales.com/adccb4/ferrari-f40-valeo-1989-giovanni-agnelli-05/1366_2000.jpeg'),
+(7, 'T-Cross', ' $427,504', '2024', 'https://www.carrosyclasicos.com/media/volkswagen_karmann_ghia_1.jpg');
 
 -- --------------------------------------------------------
 
@@ -51,6 +80,7 @@ INSERT INTO `modelos` (`id`, `Modelo`, `Precio`, `Fecha`) VALUES
 CREATE TABLE `users` (
   `id` int(100) NOT NULL,
   `Nombre` varchar(100) NOT NULL,
+  `Clave` int(100) NOT NULL,
   `user` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -58,12 +88,23 @@ CREATE TABLE `users` (
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `Nombre`, `user`) VALUES
-(1, 'Xavier', 'adm');
+INSERT INTO `users` (`id`, `Nombre`, `Clave`, `user`) VALUES
+(1, 'Xavier', 1234, 'adm'),
+(2, 'oaoao', 12345, ''),
+(3, '123', 123, ''),
+(4, '', 0, ''),
+(5, '1231', 123, ''),
+(6, '222222', 222222222, '');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `modelos`
@@ -82,16 +123,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `compras`
+--
+ALTER TABLE `compras`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT de la tabla `modelos`
 --
 ALTER TABLE `modelos`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
